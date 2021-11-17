3faa20c4d5cb57bbfd64d73f6780b89d3cb0f966
Tjipke van der Heide <tjipkevdh@gmail.com>
2021-11-17 17:01:00 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -5,23 +5,29 @@ pygame.init()
 
 class hangman():
     def __init__(self,word):
-        self.good_guesses  = 0;
-        self.wrong_guesses = 0;
+        self.good_guesses  = [];
+        self.wrong_guesses = [];
         self.word_to_guess = word
     def make_guess(self,guess):
-        if(guess in word):
-            self.good_guesses += 1
+        if(guess in self.word_to_guess):
+            print('good')
+            self.good_guesses += guess
         else:
-            self.wrong_guesses += 1
-    def draw():
-            pass
-    
+            print('bad')
+            self.wrong_guesses += guess
+    def draw(self):
+        pass
+    def get_progress(self):
+        return self.word_to_guess
+        
 screen = pygame.display.set_mode((500, 500))
-h = hangman()
-while run:
+h = hangman('baguette')
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False 
+    guess = str(input('guess'))[0]
+    h.make_guess(guess)
     h.draw()
     pygame.display.update()
 pygame.quit()
