19c9e4076820769fc69bc47f64ce241bdf3b4d94
Tjipke van der Heide <tjipkevdh@gmail.com>
2021-11-23 16:28:53 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,22 +4,34 @@ import random
 WIDTH = 500
 HEIGHT = 500
 
+FONT_SIZE = WIDTH // 10
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+#pygame.font.init()
 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+hangmanfont = pygame.font.SysFont('Comic Sans MS', FONT_SIZE)
+#steps:
+#1: rope + horizontalbeam + vertical beam + beam stand
+#2: head
+#3: body
+#4: left-arm
+#5: right-arm
+#6: left-leg
+#7: right-leg
 lines = [
         #start of vertical beam, the "pole"
         ((WIDTH / 3,HEIGHT / 6),
         ((WIDTH / 3)+10,HEIGHT / 6),
         ((WIDTH / 3)+10,(HEIGHT / 6)*5),
-        (WIDTH / 3,(HEIGHT / 6)*5)), 
-        
-        #start of horizontal beam
-        (((WIDTH / 3)+10,HEIGHT / 6),
+        (WIDTH / 3,(HEIGHT / 6)*5),
+        (WIDTH / 3,HEIGHT / 6),
+        ((WIDTH / 3)+10,HEIGHT / 6),
         ((WIDTH / 3)*2,HEIGHT / 6),
         ((WIDTH / 3)*2,(HEIGHT / 6)+10),
-        ((WIDTH / 3)+10,(HEIGHT / 6)+10)),
-            
+        ((WIDTH / 3)+10,(HEIGHT / 6)+10),
+        ((WIDTH / 3)+10,HEIGHT / 6)),
+        
+        
             ((20,20),(20,30),(30,30),(30,20)),
             
             ((20,20),(20,30),(30,30),(30,20)),
@@ -37,18 +49,24 @@ class hangman():
         self.good_guesses  = [];
         self.wrong_guesses = [];
         self.word_to_guess = word
+        
     def make_guess(self,guess):
         if(guess in self.word_to_guess):
             self.good_guesses += guess
         else:
             self.wrong_guesses += guess
+            
     def draw(self):
         for p in lines[0:len(self.wrong_guesses)]:
             pygame.draw.polygon(screen, (255,255,255),p)
+        self.show_progress()
+            
     def is_game_over(self):
         return len(self.wrong_guesses) >= 8
+    
     def is_game_won(self):
         return not '.' in self.get_progress()
+        
     def get_progress(self):
         temp = ''
         for c in self.word_to_guess:
@@ -56,17 +74,22 @@ class hangman():
                 temp += c
             else:
                 temp += '.'
-        return temp#self.word_to_guess
+        return temp
+    
+    def show_progress(self):
+        textsurface = hangmanfont.render(self.get_progress(), False, (255, 255, 255))
+        screen.blit(textsurface,(WIDTH/3,HEIGHT-40))
         
 h = hangman('baguette')
 while not h.is_game_over() and not h.is_game_won():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    guess = str(input('guess'))[0]
-    h.make_guess(guess)
-    h.draw()
-    print(h.get_progress())
-    pygame.display.update()
+        if event.type == pygame.KEYDOWN:
+            screen.fill((0,0,0))
+            h.make_guess(pygame.key.name(event.key))
+            h.draw()
+            pygame.display.update()
+   
 
 pygame.quit()
